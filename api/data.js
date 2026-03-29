eb77c84e07c5c71cb7a125f2e2b4ae925c06561a
OneSep <112628881+OneSep@users.noreply.github.com>
2026-03-29 23:33:42 +0900

    Update fetch URL in data handler function

--- api/data.js
+++ api/data.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const response = await fetch("여기에_AppsScript_URL");
+  const response = await fetch("https://script.google.com/macros/s/AKfycbzfJSm1yqp6PeagvmTEOLymyO1EejYo64mj3BuSoV3tytsPLnrltOf33zUxUk4YpC1v/exec");
   const data = await response.json();
 
   res.setHeader("Cache-Control", "s-maxage=60");

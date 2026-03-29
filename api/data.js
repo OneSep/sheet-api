723cab2f8ad5111125d8d9feb8707c525fa1c318
OneSep <112628881+OneSep@users.noreply.github.com>
2026-03-29 23:20:19 +0900

    Add API handler to fetch and return JSON data

--- api/data.js
+++ api/data.js
@@ -1,1 +1,7 @@
+export default async function handler(req, res) {
+  const response = await fetch("여기에_AppsScript_URL");
+  const data = await response.json();
 
+  res.setHeader("Cache-Control", "s-maxage=60");
+  res.status(200).json(data);
+}

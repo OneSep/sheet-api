536c788eae07fd67d3142431421d59449838ee04
OneSep <112628881+OneSep@users.noreply.github.com>
2026-03-29 23:34:11 +0900

    Update data.js

--- api/data.js
+++ api/data.js
@@ -2,6 +2,6 @@ export default async function handler(req, res) {
   const response = await fetch("https://script.google.com/macros/s/AKfycbzfJSm1yqp6PeagvmTEOLymyO1EejYo64mj3BuSoV3tytsPLnrltOf33zUxUk4YpC1v/exec");
   const data = await response.json();
 
-  res.setHeader("Cache-Control", "s-maxage=60");
+  res.setHeader("Cache-Control", "s-maxage=30");
   res.status(200).json(data);
 }

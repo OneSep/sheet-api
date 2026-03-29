6a1ba2c47e6bbe1577e608816dfb6725382dc203
OneSep <112628881+OneSep@users.noreply.github.com>
2026-03-30 02:08:07 +0900

    Add redirect option to fetch request

--- api/data.js
+++ api/data.js
@@ -1,7 +1,10 @@
 export default async function handler(req, res) {
-  const response = await fetch("https://script.google.com/macros/s/AKfycbzfJSm1yqp6PeagvmTEOLymyO1EejYo64mj3BuSoV3tytsPLnrltOf33zUxUk4YpC1v/exec");
+  const response = await fetch("https://script.google.com/macros/s/AKfycbzfJSm1yqp6PeagvmTEOLymyO1EejYo64mj3BuSoV3tytsPLnrltOf33zUxUk4YpC1v/exec", {
+    redirect: "follow"
+  });
   const data = await response.json();
 
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Cache-Control", "s-maxage=30");
   res.status(200).json(data);
 }
